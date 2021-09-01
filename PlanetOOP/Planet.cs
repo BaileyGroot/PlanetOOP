d33ef0dcaf2442f2df9c51321805f236c06b4df9
Grootrix <grootrix@gmail.com>
2021-09-01 11:57:14 +0200

    Add project files.

---
+++ PlanetOOP/Planet.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetOOP
+{
+    class Planet
+    {
+        // Properties
+        string name;
+        double mass;
+        double diameter;
+        double density;
+        double gravity;
+        double rotationperiod;
+        double lenthofday;
+        double distancefromsun;
+        double orbitalperiod;
+        double obitalvelocity;
+        double meantemperture;
+        double numberofmoons;
+        string ringsystem;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public double Mass
+        {
+            get
+            {
+                return mass;
+            }
+            set
+            {
+                mass = value;
+            }
+        }
+        public double Diameter
+        {
+            get
+            {
+                return diameter;
+            }
+            set
+            {
+                diameter = value;
+            }
+        }
+        public double Density
+        {
+            get
+            {
+                return density;
+            }
+            set
+            {
+                density = value;
+            }
+        }
+        public double Gravity
+        {
+            get
+            {
+                return gravity;
+            }
+            set
+            {
+                gravity = value;
+            }
+        }
+        public double RotationPeriod
+        {
+            get
+            {
+                return rotationperiod;
+            }
+            set
+            {
+                rotationperiod = value;
+            }
+        }
+        public double LenthofDay
+        {
+            get
+            {
+                return lenthofday;
+            }
+            set
+            {
+                lenthofday = value;
+            }
+        }
+        public double DistancefromSun
+        {
+            get
+            {
+                return distancefromsun;
+            }
+            set
+            {
+                distancefromsun = value;
+            }
+        }
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return orbitalperiod;
+            }
+            set
+            {
+                orbitalperiod = value;
+            }
+        }
+        public double ObitalVelocity
+        {
+            get
+            {
+                return obitalvelocity;
+            }
+            set
+            {
+                obitalvelocity = value;
+            }
+        }
+        public double MeanTemperture
+        {
+            get
+            {
+                return meantemperture;
+            }
+            set
+            {
+                meantemperture = value;
+            }
+        }
+        public double NumberofMoon
+        {
+            get
+            {
+                return numberofmoons;
+            }
+            set
+            {
+                numberofmoons = value;
+            }
+        }
+        public string RingSystem
+        {
+            get
+            {
+                return ringsystem;
+            }
+            set
+            {
+                ringsystem = value;
+            }
+        }
+
+        // Constructors
+        public Planet(string Name, double Mass, double Diameter, double Density, double Gravity,
+        double RotationPeriod, double LenthofDay, double DistancefromSun, double OrbitalPeriod,
+        double ObitalVelocity, double MeanTemperture, double NumberofMoons, string RingSystem)
+        {
+            this.Name = Name;
+            this.Mass = Mass;
+            this.Diameter = Diameter;
+            this.Density = Density;
+            this.Gravity = Gravity;
+            this.RotationPeriod = RotationPeriod;
+            this.LenthofDay = LenthofDay;
+            this.DistancefromSun = DistancefromSun;
+            this.OrbitalPeriod = OrbitalPeriod;
+            this.ObitalVelocity = ObitalVelocity;
+            this.MeanTemperture = MeanTemperture;
+            this.NumberofMoon = NumberofMoons;
+            this.RingSystem = RingSystem;
+
+
+        }
+
+
+
+        // Methods
+
+    }
+}
---
+++ PlanetOOP/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Planet> planets = new List<Planet>();
+
+
+
+            Planet Mecury = new Planet("Mecury", 0.330, 4879, 5427, 3.7, 1407.6, 4222.6, 57.9, 88.0, 47.4, 167, 0, "No");
+            planets.Add(Mecury);
+
+            Planet Venus = new Planet("Venus", 0.330, 4879, 5427, 3.7, 1407.6, 4222.6, 57.9, 88.0, 47.4, 167, 0, "no");
+            planets.Add(Venus);
+
+            Planet Earth = new Planet("Earth", 5.97, 12756, 5514, 9.8, 23.9, 24.0, 149.6, 365.2, 29.8, 15, 1, "No");
+            planets.Add(Earth);
+
+            Planet Mars = new Planet("Mars", 0.642, 6792, 3933, 3.7, 24.6, 24.7, 227.9, 687.0, 24.1, -65, 2, "No");
+            planets.Add(Mars);
+
+            Planet Jupiter = new Planet("Jupiter", 1898, 142984, 1326, 23.1, 9.9, 9.9, 778.6, 4431, 13.1, -110, 67, "Yes");
+            planets.Add(Jupiter);
+
+            Planet Saturn = new Planet("Saturn", 568, 120536, 687, 9.0, 10.7, 10.7, 1433.5, 10747, 9.7, -140, 62, "Yes");
+            planets.Add(Saturn);
+
+            Planet Uranus = new Planet("Uranus", 86.8, 51118, 1271, 8.7, -17.2, 17.2, 2872.5, 30589, 6.8, -195, 27, "Yes");
+            planets.Add(Uranus);
+
+            Planet Neptune = new Planet("Neptune", 102, 49528, 1638, 10.0, 16.1, 16.1, 4495.1, 59.8, 5.4, -200, 14, "Yes");
+            planets.Add(Neptune);
+
+            Planet Pluto = new Planet("Pluto", 0.0146, 2370, 2095, 0.7, -153.3, 153.3, 5906.4, 90.56, 4.7, -255, 5, "No");
+            planets.Add(Pluto);
+
+            for (int i = 0; i < planets.Count; i++)
+            {
+                if (planets[i] == Pluto)
+                {
+                    planets.Remove(Pluto);
+                }
+            }
+
+            Planet Pluto1 = new Planet("Pluto", 0.0146, 2370, 2095, 0.7, -153.3, 153.3, 5906.4, 90.56, 4.7, -255, 5, "No");
+            planets.Add(Pluto1);
+
+
+            //foreach (var planet in planets)
+            //{
+            //    Console.WriteLine(planet.Name);
+            //}
+
+            //Console.WriteLine("There are " + planets.Count + " elements");
+
+
+            List<Planet> mean = new List<Planet>();
+
+            for (int i = 0; i < planets.Count; i++)
+            {
+                if (planets[i].MeanTemperture < 0)
+                {
+                    mean.Add(planets[i]);
+                    
+                }
+            }
+
+            List<Planet> diame = new List<Planet>();
+
+            for (int i = 0; i < planets.Count; i++)
+            {
+                if (planets[i].Diameter >= 10000 && planets[i].Diameter < 50000 )
+                {
+                    diame.Add(planets[i]);
+                }
+            }
+
+            planets.Clear();
+            diame.Clear();
+            mean.Clear();
+
+            Console.WriteLine(planets);
+            Console.WriteLine();
+            
+
+
+                Console.ReadKey();
+            
+        }
+    }
+}
